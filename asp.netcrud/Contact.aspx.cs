50f59d2bbe5650625d6d773ffe14fe7d1eddc17b
Yolanda Charles <technosayge@gmail.com>
2018-07-10 15:35:39 -0400

    CRUD UPdate
    Added functionality

--- asp.netcrud/Contact.aspx.cs
+++ asp.netcrud/Contact.aspx.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -9,6 +11,9 @@ namespace asp.netcrud
 {
     public partial class Contact : System.Web.UI.Page
     {
+        //SqlConnection sqlCon = new SqlConnection(@"Data Source=(local)\sqle2012;Initial Catalog=ASPCRUD;Integrated Security=true");
+        SqlConnection sqlCon = new SqlConnection(@"Data Source=;Initial Catalog=Yolanda;Integrated Security=SSPI");
+        //CLUSTERD1\DEVSQLC1
         protected void Page_Load(object sender, EventArgs e)
         {
             if(!IsPostBack)
@@ -27,6 +32,24 @@ namespace asp.netcrud
             lblSuccessMessage.Text = lblErrorMessage.Text = "";
             btnSave.Text = "Save";
             btnDelete.Enabled = false;
+        }
+        protected void btnSave_Click(object sender, EventArgs e)
+        {
+            if (sqlCon.State == ConnectionState.Closed)
+                sqlCon.Open();
+            SqlCommand sqlCmd = new SqlCommand("ContactCreateOrUpdate", sqlCon);
+            sqlCmd.CommandType = CommandType.StoredProcedure;
+            sqlCmd.Parameters.AddWithValue("@ContactID", (hfContactID.Value==""?0:Convert.ToInt32(hfContactID.Value)));
+            sqlCmd.Parameters.AddWithValue("@name", txtName.Text.Trim());
+            sqlCmd.Parameters.AddWithValue("@mobile", txtMobile.Text.Trim());
+            sqlCmd.Parameters.AddWithValue("@address", txtAddress.Text.Trim());
+            sqlCmd.ExecuteNonQuery();
+            sqlCon.Close();
+            Clear();
+            if (hfContactID.Value == "")
+                lblSuccessMessage.Text = "Saved Succesfully!!";
+            else
+                lblSuccessMessage.Text = "Updated Successfully!";
         }
     }
 }
